21c2b5240e95393546add875a94f7a5b255f20b8
Shreya Malviya <malviyashreya26@gmail.com>
2021-06-12 23:43:40 +0530

    [Shreya] Add: Check the max(float) Of 3 number

--- FindMaxUsingGenerics/FindMaxUsingGenerics/FindMaxUsingGenerics.cs
+++ FindMaxUsingGenerics/FindMaxUsingGenerics/FindMaxUsingGenerics.cs
@@ -23,6 +23,21 @@ namespace FindMaxUsingGenerics
                 return thirdValue;
 
         }
+        //check Foat Maximum Float Number
+        public static double MaximumFloatNumber(double firstValue, double secondValue, double thirdValue)
+        {
+            //comapring First Number to Second Number > 0  and firstNumber to third Number >0  
+            if (firstValue.CompareTo(secondValue) > 0 && firstValue.CompareTo(thirdValue) > 0)
+                //return 
+                return firstValue;
+            //comapring Second Number to First Number  > 0  and SecondNumber to third Number >0  
+            if (secondValue.CompareTo(firstValue) > 0 && secondValue.CompareTo(thirdValue) > 0)
+                //return secondNumber
+                return secondValue;
+            else
+                //return thirdNumber
+                return thirdValue;
+        }
 
     }
 }
